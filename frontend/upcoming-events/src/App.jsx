773935026722466a37b5a9259008507d1c42d949
Komal <ks989156@gmail.com>
2025-05-30 19:34:54 +0530

    UI changes

--- frontend/upcoming-events/src/App.jsx
+++ frontend/upcoming-events/src/App.jsx
@@ -30,27 +30,30 @@ function App() {
           {
             events.slice().reverse().map((item,index)=>(
             <div key={index} className='border mb-4 border-b border-white/20 bg-white/15 lg:mx-4 p-2 rounded-2xl'>
-              <div className='flex gap-4 items-center mx-4'>
+              <div className='flex gap-4 items-center mx-4 mb-1'>
                 <h4 className='text-xl font-bold'>{item.league_name}</h4>
                 <h4 className='text-lg font-light'>{item.league_season}</h4>
               </div>
 
-              <div className='flex flex-col items-center px-4'>
-                <div className='flex justify-between w-full p-2'>
-                  <div>
-                    <img className='w-12 h-12 rounded-full' src={item.event_home_team_logo} alt="home teamlogo" />
+              <div className='flex flex-col items-center px-10'>
+                <div className='flex justify-between w-full py-2'>
+                  <div className='w-2/12'>
+                    <img className='w-10 h-10 rounded-full mb-1' src={item.event_home_team_logo} alt="home teamlogo" />
                     <h4 className='text-lg font-light p-1'>{item.event_home_team}</h4>
                   </div>
-                  <div className='flex items-center'>
-                    <p className='text-2xl font-extrabold italic  bg-gradient-to-r from-[#2370c9] to-[#c4375a] bg-clip-text text-transparent'>V/S</p>
+
+                  <div className='flex items-center justify-center w-2/12 '>
+                    <p className='text-3xl font-extrabold italic  bg-gradient-to-r from-[#2370c9] to-[#c4375a] bg-clip-text text-transparent'>V/S</p>
                   </div>
-                  <div className='flex flex-col items-end'>
-                    <img className='w-12 h-12 rounded-full' src={item.event_away_team_logo} alt="team logo" />
+
+                  <div className='flex flex-col items-end w-2/12'>
+                    <img className='w-10 h-10 rounded-full mb-1' src={item.event_away_team_logo} alt="team logo" />
                     <h4 className='text-lg font-light p-1'>{item.event_away_team}</h4>
                   </div>
                 </div>
               </div>
-              <p className='flex justify-center items-center text-base font-light border-t border-white/20 pt-2 mx-6'>{item.event_date} at {item.event_time}</p>
+
+              <p className='flex justify-center items-center text-base font-light border-t border-white/20 pt-2 mx-10'>{item.event_date} at {item.event_time}</p>
 
               </div>
             ))
