63ae53026911ee2533fdab0883e79904dad4009b
Komal <ks989156@gmail.com>
2025-05-30 18:51:10 +0530

    fix env file err

--- frontend/upcoming-events/src/App.jsx
+++ frontend/upcoming-events/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const url = import.meta.env.VITE_API_BASE_URL
 
   useEffect(()=>{
-    axios.get('https://basketball-fixtures.onrender.com/events')
+    axios.get(`${url}/events`)
     .then((response)=>{
       console.log(response.data)
       setEvents(response.data)
